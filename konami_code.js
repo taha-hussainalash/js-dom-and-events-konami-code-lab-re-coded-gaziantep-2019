94040844b3fac166aef97d75091835402aef779a
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-18 14:09:46 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-let index = 0;
+  
+  let index = 0;
 
   document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.which || e.detail);
